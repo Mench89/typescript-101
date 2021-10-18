21b639a57a82b1415829ad1519659e6f4af2f6ae
Magnus Lindberg <magnus.lindberg89@gmail.com>
2021-10-18 10:02:43 +0200

    Initial commit

---
+++ 01-getting-started/01-variables.ts
@@ -0,0 +1,9 @@
+// Variable that cannot be updated after initialization
+const name = "John"
+console.log(name) // John
+
+// Variable that can be updated after initialization
+let age = 42
+console.log(age) // 42
+
+export {}
